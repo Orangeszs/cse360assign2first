afeb0b1b04044cb857744e642ed6928735849d9b
thais <thais@DESKTOP-F228OC0>
2019-02-25 23:54:08 -0700

    First Change

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,32 +1,38 @@
 
 package cse360assign2;
-
+/**
+ * Calculates simple calculations, such as adding, subtracting, multiplication, division, and other methods pertaining to history of calculations.
+ * @author thais, id 404, 1212866926
+ * @param total Used for getTotal,holds the total value 
+ *
+ */
 public class Calculator {
 
 	private int total;
-	
+
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
 	}
 	
 	public void multiply (int value) {
-		
+		total = total * value;
 	}
 	
 	public void divide (int value) {
-		
+		if (value != 0) total = total / value;
+		else total = 0;
 	}
 	
 	public String getHistory () {
